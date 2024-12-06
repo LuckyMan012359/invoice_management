5109ba9b2b0dcb9f1f6ad3386611c4a0fd36bac8
LuckyMan012359 <rorifloris@gmail.com>
2024-12-06 19:46:38 +0000

    fix: update get user role api function

--- client/src/utils/getUserRole.js
+++ client/src/utils/getUserRole.js
@@ -3,7 +3,7 @@ import axiosInstance from './axiosInstance';
 const getUserRole = async () => {
   const response = await axiosInstance('/user/get_user_info', 'get');
 
-  if (response.data.user.role) {
+  if (response.data.user) {
     return response.data.user.role;
   } else {
     return '';
