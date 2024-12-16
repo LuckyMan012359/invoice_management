ee3c1ce9c1437c00223db1a9201948922409da04
LuckyMan012359 <rorifloris@gmail.com>
2024-12-16 17:25:51 +0000

    feat: add badge to tab

--- client/src/pages/Transaction/Transactions.jsx
+++ client/src/pages/Transaction/Transactions.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useState } from 'react';
-import { Tabs } from 'antd';
+import { Badge, Tabs } from 'antd';
 import { TransactionTable } from '../../components/Tables/transactionTable';
 import { TransactionPendingTable } from '../../components/Tables/transactionPendingTable';
 import { TransactionCreateApproveTable } from '../../components/Tables/transactionCreateApproveTable';
@@ -13,37 +13,86 @@ export const Transactions = () => {
   const { t } = useTranslation();
 
   const [isChanged, setIsChanged] = useState(false);
+  const [tabNum, setTabNum] = useState('1'); // Tracks the selected tab
 
   return (
     <div className='min-h-screen px-[100px] pb-[50px] pt-[200px] max-xl:px-[50px] max-sm:px-[15px] bg-gray-100 dark:bg-gray-900'>
       <div className='mx-auto bg-white shadow-md rounded-lg p-6 space-y-6 dark:bg-gray-800'>
-        <Tabs defaultActiveKey='1'>
+        <Tabs
+          activeKey={tabNum}
+          onChange={(key) => setTabNum(key)} // Updates the active tab
+        >
           <TabPane
-            tab={t('Transactions')}
+            tab={
+              <Badge
+                dot={false}
+                className={`dark:!text-[#fff] ${
+                  tabNum === '1'
+                    ? '!text-[#1677ff] dark:!text-[#1677ff]'
+                    : '!text-[#000] dark:!text-[#fff]'
+                }`}
+              >
+                {t('Transactions')}
+              </Badge>
+            }
             key='1'
-            className='!text-[#000] dark:!text-[#fff]'
-            style={{ color: '#444' }}
           >
             <TransactionTable isChanged={isChanged} setIsChanged={setIsChanged} />
           </TabPane>
           <TabPane
-            tab={t('Pending Transactions')}
+            tab={
+              <Badge
+                count={10}
+                showZero={false}
+                overflowCount={99}
+                className={`dark:!text-[#fff] ${
+                  tabNum === '2'
+                    ? '!text-[#1677ff] dark:!text-[#1677ff]'
+                    : '!text-[#000] dark:!text-[#fff]'
+                }`}
+              >
+                {t('Pending Transactions')}
+              </Badge>
+            }
             key='2'
-            className='!text-[#000] dark:!text-[#fff]'
           >
             <TransactionPendingTable isChanged={isChanged} setIsChanged={setIsChanged} />
           </TabPane>
           <TabPane
-            tab={t('Approve Create Transaction')}
+            tab={
+              <Badge
+                count={10}
+                showZero={false}
+                overflowCount={99}
+                className={`dark:!text-[#fff] ${
+                  tabNum === '3'
+                    ? '!text-[#1677ff] dark:!text-[#1677ff]'
+                    : '!text-[#000] dark:!text-[#fff]'
+                }`}
+              >
+                {t('Approve Create Transaction')}
+              </Badge>
+            }
             key='3'
-            className='!text-[#000] dark:!text-[#fff]'
           >
             <TransactionCreateApproveTable isChanged={isChanged} setIsChanged={setIsChanged} />
           </TabPane>
           <TabPane
-            tab={t('Approve Update Transaction')}
+            tab={
+              <Badge
+                count={10}
+                showZero={false}
+                overflowCount={99}
+                className={`dark:!text-[#fff] ${
+                  tabNum === '4'
+                    ? '!text-[#1677ff] dark:!text-[#1677ff]'
+                    : '!text-[#000] dark:!text-[#fff]'
+                }`}
+              >
+                {t('Approve Update Transaction')}
+              </Badge>
+            }
             key='4'
-            className='!text-[#000] dark:!text-[#fff]'
           >
             <TransactionUpdateApproveTable isChanged={isChanged} setIsChanged={setIsChanged} />
           </TabPane>
