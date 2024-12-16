f4e242a70e04265045939ee5c27c1ac6ff4217d0
LuckyMan012359 <rorifloris@gmail.com>
2024-12-16 20:03:25 +0000

    feat: complete integrating with backend

--- client/src/pages/Transaction/Transactions.jsx
+++ client/src/pages/Transaction/Transactions.jsx
@@ -13,14 +13,31 @@ const { TabPane } = Tabs;
 export const Transactions = () => {
   const backendDomain = process.env.REACT_APP_SOCKET_URL || 'wss://negociationalex.lat/api';
 
-  console.log(backendDomain);
+  const [approveCreateTransactionAmount, setApproveCreateTransactionAmount] = useState(0);
+  const [approveUpdateTransactionAmount, setApproveUpdateTransactionAmount] = useState(0);
+  const [pendingTransactionAmount, setPendingTransactionAmount] = useState(0);
 
   const socket = io(backendDomain, () => {
     console.log('Websocket connected');
   });
 
   socket.on('transactionDataUpdated', (data) => {
-    console.log('Updated Transaction Data:', data);
+    if (data && data.transactions && data.transactions.length >= 0) {
+      const approveCreateTransaction = data.transactions.filter(
+        (item) => Number(item.approve_status) === 2,
+      );
+
+      const approveUpdateTransaction = data.transactions.filter(
+        (item) => Number(item.approve_status) === 3,
+      );
+
+      setApproveCreateTransactionAmount(approveCreateTransaction.length || 0);
+      setApproveUpdateTransactionAmount(approveUpdateTransaction.length || 0);
+    }
+
+    if (data && data.pendingTransactions && data.pendingTransactions.length >= 0) {
+      setPendingTransactionAmount(data.pendingTransactions.length || 0);
+    }
   });
 
   const { t } = useTranslation();
@@ -29,7 +46,7 @@ export const Transactions = () => {
   const [tabNum, setTabNum] = useState('1');
   return (
     <div className='min-h-screen px-[100px] pb-[50px] pt-[200px] max-xl:px-[50px] max-sm:px-[15px] bg-gray-100 dark:bg-gray-900'>
-      <div className='mx-auto bg-white shadow-md rounded-lg p-6 space-y-6 dark:bg-gray-800'>
+      <div className='tabs mx-auto bg-white shadow-md rounded-lg p-6 space-y-6 dark:bg-gray-800'>
         <Tabs
           activeKey={tabNum}
           onChange={(key) => setTabNum(key)} // Updates the active tab
@@ -43,6 +60,7 @@ export const Transactions = () => {
                     ? '!text-[#1677ff] dark:!text-[#1677ff]'
                     : '!text-[#000] dark:!text-[#fff]'
                 }`}
+                size='small'
               >
                 {t('Transactions')}
               </Badge>
@@ -54,7 +72,7 @@ export const Transactions = () => {
           <TabPane
             tab={
               <Badge
-                count={10}
+                count={pendingTransactionAmount}
                 showZero={false}
                 overflowCount={99}
                 className={`dark:!text-[#fff] ${
@@ -62,6 +80,7 @@ export const Transactions = () => {
                     ? '!text-[#1677ff] dark:!text-[#1677ff]'
                     : '!text-[#000] dark:!text-[#fff]'
                 }`}
+                size='small'
               >
                 {t('Pending Transactions')}
               </Badge>
@@ -73,7 +92,7 @@ export const Transactions = () => {
           <TabPane
             tab={
               <Badge
-                count={10}
+                count={approveCreateTransactionAmount}
                 showZero={false}
                 overflowCount={99}
                 className={`dark:!text-[#fff] ${
@@ -81,6 +100,7 @@ export const Transactions = () => {
                     ? '!text-[#1677ff] dark:!text-[#1677ff]'
                     : '!text-[#000] dark:!text-[#fff]'
                 }`}
+                size='small'
               >
                 {t('Approve Create Transaction')}
               </Badge>
@@ -92,7 +112,7 @@ export const Transactions = () => {
           <TabPane
             tab={
               <Badge
-                count={10}
+                count={approveUpdateTransactionAmount}
                 showZero={false}
                 overflowCount={99}
                 className={`dark:!text-[#fff] ${
@@ -100,6 +120,7 @@ export const Transactions = () => {
                     ? '!text-[#1677ff] dark:!text-[#1677ff]'
                     : '!text-[#000] dark:!text-[#fff]'
                 }`}
+                size='small'
               >
                 {t('Approve Update Transaction')}
               </Badge>
